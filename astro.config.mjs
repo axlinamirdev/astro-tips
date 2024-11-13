f942d5643800863230dee40d7e43bb4ea462bb68
Axlin Miranda <axlina90@gmail.com>
2024-11-12 20:43:00 -0400

    feat: configuracion del proyecto en astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
+import svelte from '@astrojs/svelte';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://localhost:3000',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
+});
